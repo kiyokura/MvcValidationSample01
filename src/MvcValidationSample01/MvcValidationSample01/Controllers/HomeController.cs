e9424824e76fc041079d53e9070d4b5e17aff311
Kiyokura Narami <kiyokura@gmail.com>
2014-07-09 00:37:13 +0900

    First Commit

---
+++ src/MvcValidationSample01/MvcValidationSample01/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcValidationSample01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ModelState.AddModelError("foo", "error for foo 1");
+            ModelState.AddModelError("foo", "error for foo 2");
+            ModelState.AddModelError("bar", "error for bar");
+            ModelState.AddModelError("hoge", "error for hoge");
+            return View();
+        }
+    }
+}
---
+++ src/MvcValidationSample01/MvcValidationSample01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcValidationSample01.MvcApplication" Language="C#" %>
